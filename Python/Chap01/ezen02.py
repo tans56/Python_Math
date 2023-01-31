dbe49c3b47e6ad0f04a4a02ed2e331d1e064cb03
chanhee0506 <chyoo98@naver.com>
2023-01-31 17:15:07 +0900

    20230131화

---
+++ Python/Chap01/ezen02.py
@@ -0,0 +1,17 @@
+'''
+    * turtle graphic 모듈
+'''
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+# 앞으로 전진
+t.forward(100)
+# 왼쪽으로 90도 회전
+t.left(90)
+
+t.forward(50)
+
+
+
+turtle.exitonclick()
